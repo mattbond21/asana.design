e63c74a23f1f912715d3774633501cff98b97f29
Matt Bond <matt@mattbond.com.au>
2018-09-08 16:39:32 -0700

    updates with devins feedback

--- js/main.js
+++ js/main.js
@@ -84,5 +84,19 @@
       $(this).parent().removeClass('demo-video-paused');
   });
 
+  // Sticky design team sidebar
+
+  var stick = $("#stick");
+
+  stick.on("scroll", function(e) {
+
+    if (this.scrollTop > 1000) {
+      stick.addClass("sticky-sidebar");
+    } else {
+      stick.removeClass("sticky-sidebar");
+    }
+
+  });
+
 
 }(jQuery);
