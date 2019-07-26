039e8b9984ae45e003175e55c85b4355dd482ada
Brandon Chang <brandonchang@asana.com>
2019-07-26 15:01:14 -0700

    Fixes double stamp on mobile, sticky on safari

--- js/stamps.js
+++ js/stamps.js
@@ -1,4 +1,4 @@
-let number = 0
+let stampIndex = 0;
 const stamps = [
   "../images/stamps/dice.png",
   "../images/stamps/face.png",
@@ -12,36 +12,31 @@ const stamps = [
   "../images/stamps/horn.png",
   "../images/stamps/plane.png",
   "../images/stamps/sunny.png"
-]
+];
 
-const stampsTag = document.querySelector("div.stamps")
+const stampsTag = document.querySelector(".stamps");
 
 const addStamp = function (x, y) {
-	// <img src="circles.svg">
-  const img = document.createElement("img")
-  img.setAttribute("src", stamps[number])
+  const img = document.createElement("img");
+  img.setAttribute("src", stamps[stampIndex]);
 
 	// remove half the window width
-  img.style.left = (x - window.innerWidth / 2) + "px"
-  img.style.top = y + "px"
+  img.style.left = (x - window.innerWidth / 2) + "px";
+  img.style.top = y + "px";
 
-  stampsTag.appendChild(img)
+  stampsTag.appendChild(img);
 
 	// add some audio. can add multiple similar to how stamps were added
-  const audio = document.createElement("audio")
-  audio.setAttribute("src", "images/stamps/pop.mp3")
-  audio.play()
+  const audio = document.createElement("audio");
+  audio.setAttribute("src", "images/stamps/pop.mp3");
+  audio.play();
 
-  number = number + 1
-  if (number > stamps.length - 1) {
-    number = 0
+  stampIndex++;
+  if (stampIndex > stamps.length - 1) {
+    stampIndex = 0
   }
-}
+};
 
 document.addEventListener("click", function(event) {
   addStamp(event.pageX, event.pageY)
-}, false)
-
-document.addEventListener("touchend", function(event) {
-  addStamp(event.pageX, event.pageY)
-}, false)
+}, false);
